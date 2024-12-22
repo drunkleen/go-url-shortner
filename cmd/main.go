d8c67a3a77acec6efe82d64af0df127661e90d3a
DrunkLeen <drunkleen@gmail.com>
2024-12-22 03:07:47 +0100

    Url Forwarding Final

--- cmd/main.go
+++ cmd/main.go
@@ -3,23 +3,32 @@ package main
 import (
 	"log"
 
+	"github.com/drunkleen/go-url-shortner/config"
+	"github.com/drunkleen/go-url-shortner/handler"
+	"github.com/drunkleen/go-url-shortner/store"
 	"github.com/gin-gonic/gin"
-)
-
-const (
-	PORT string = ":8080"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello World!",
+			"message": "Welcome to the URL Shortener API",
 		})
 	})
 
-	log.Printf("Server is running on port %s", PORT)
-	if err := r.Run(PORT); err != nil {
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	store.InitializeStoreService()
+
+	log.Printf("Server is running on port %s", config.Port)
+	if err := r.Run(config.Port); err != nil {
 		log.Fatalf("Failed to run server: %v", err)
 	}
 }
---
+++ handler/handlers.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/drunkleen/go-url-shortner/config"
+	"github.com/drunkleen/go-url-shortner/shortener"
+	"github.com/drunkleen/go-url-shortner/store"
+	"github.com/gin-gonic/gin"
+)
+
+type UrlCreationRequest struct {
+	LongUrl string `json:"long_url" binding:"required"`
+	UserId  string `json:"user_id" binding:"required"`
+}
+
+func CreateShortUrl(c *gin.Context) {
+	var creationRequest UrlCreationRequest
+	if err := c.ShouldBindJSON(&creationRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	shortUrl := shortener.GenerateShortLink(creationRequest.LongUrl, creationRequest.UserId)
+	store.SaveUrlMapping(shortUrl, creationRequest.LongUrl, creationRequest.UserId)
+
+	host := fmt.Sprintf("http://localhost%s/", config.Port)
+	c.JSON(200, gin.H{
+		"message":   "short url created successfully",
+		"short_url": host + shortUrl,
+	})
+}
+
+func HandleShortUrlRedirect(c *gin.Context) {
+	shortUrl := c.Param("shortUrl")
+	initialUrl := store.RetrieveInitialUrl(shortUrl)
+	c.Redirect(302, initialUrl)
+}
